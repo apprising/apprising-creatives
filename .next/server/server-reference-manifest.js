634294f265b120376027d435137b04c890e9fb67
apprising <apprising.creatives@gmail.com>
2024-04-17 21:59:39 +0800

    remove not used homepages

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"r6ZF3p6C/oU2O9o/9kehKeQygLEc58cMFT52U4QAukU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OZag4dHiS6Jw+kAx+EVbDhDH+PolbAvHnWD7x3KBCcw=\"\n}"
